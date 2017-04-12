dd82214d1227e5a7b817902b3bc1da0dd274a25b
andrelima1039 <andrevlima@outlook.com.br>
2017-04-12 15:06:24 -0300

    Criação da classe Conta contendo as propriedades numero, titulo, e saldo metodos Deposita, Saca, e transfere
    Criação de um form contendo um botão que instancia a classe conta, executa o  metodo de deposito e saca

---
+++ Banco/Conta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    class Conta
+    {
+        public Double Saldo { get; set; }
+        public String Titular { get; set; }
+        public int Numero { get; set; }
+
+        public void Deposita(double valor)
+        {
+            this.Saldo += valor;
+        }
+
+        public bool Saca(double valor)
+        {
+            if(this.Saldo > valor)
+            {
+                this.Saldo -= valor;
+                return true;
+            }
+            return false;
+        }
+
+        public void Transfere(double valor,Conta destino)
+        {
+            this.Saca(valor);
+            destino.Deposita(valor);
+        }
+    }
+}
---
+++ Banco/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banco
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conta contaAndre = new Conta();
+            contaAndre.Titular = "André";
+            contaAndre.Numero = 1;
+            contaAndre.Deposita(100);
+            if (contaAndre.Saca(500))
+            {
+                MessageBox.Show("Saque bem sucedido");
+            }
+            else
+            {
+                MessageBox.Show("Saldo Insuficiente");
+            }
+            MessageBox.Show("Saldo: "+contaAndre.Saldo);
+        }
+    }
+}
